4d025d1660578a5604c392ad944a329a1cee6f01
SashkaToropov <toropov.oleksandr03@gmail.com>
2024-01-02 18:39:57 +0200

    Changed isReal method to check the words has more than 3 letters

--- 5.WordScramble/ViewController.swift
+++ 5.WordScramble/ViewController.swift
@@ -112,6 +112,11 @@ class ViewController: UITableViewController {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        if word.count < 3 {
+            return false
+        }
+        
         return misspelledRange.location == NSNotFound
     }
 }
